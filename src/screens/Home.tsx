8348dd15693d1ca08e67023def641aefa6d8dd58
Eko Mardiatno <ekomardiatno@gmail.com>
2025-05-16 21:51:31 +0700

    feat: first setup and pod install

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { JSX } from "react"
+import { SafeAreaView, Text } from "react-native"
+
+export default function Home(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Hello world!</Text>
+    </SafeAreaView>
+  )
+}
